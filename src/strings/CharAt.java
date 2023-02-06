9b5977ebe08f97887f1d729490282434ced7a2ed
gunavardhan <gunamirthipati@gmail.com>
2023-02-06 22:26:08 +0530

    2 commit

--- src/strings/CharAt.java
+++ src/strings/CharAt.java
@@ -1,10 +1,11 @@
 package strings;
 
 public class CharAt {
-    public class CharAtExample{
-        public static void main(String args[]){
-            String name="javatpoint";
-            char ch=name.charAt(4);//returns the char value at the 4th index
+    public class CharAtExample {
+        public static void main(String args[]) {
+            String name = "javatpoint";
+            char ch = name.charAt(4);//returns the char value at the 4th index
             System.out.println(ch);
         }
+    }
 }
